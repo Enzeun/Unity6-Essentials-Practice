7c5ffb75886f18991f1d0ccba40a9d14c520c0dd
Enzeun <khuynjun123@naver.com>
2026-07-16 15:27:42 +0900

    [feat] odin 추가, bullet Spawner 추가

--- Example1_Dodge/Assets/Script/Bullet.cs
+++ Example1_Dodge/Assets/Script/Bullet.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class Bullet : MonoBehaviour
 {
     public float speed = 8f;
---
+++ Example1_Dodge/Assets/Script/OdinPractice.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+public class OdinPractice : MonoBehaviour
+{
+    [Title("디버깅 연습")]
+
+    [ShowInInspector]    
+    private bool show;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
